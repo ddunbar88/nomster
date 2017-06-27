99be7b941b8429c661e7448ab8bfa7b530b3fb0e
ddunbar88 <derekdunbar@mac.com>
2017-06-27 04:58:30 +0000

    Lesson 8.28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :name, :address, :description, presence: true
 def self.search(term, page)
   if term
     where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 5).order('id DESC')
