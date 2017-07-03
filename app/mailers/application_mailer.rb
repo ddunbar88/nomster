d36662cf0b4023cf0c91a816b3b231ffb2e434fe
ddunbar88 <derekdunbar@mac.com>
2017-07-03 04:55:35 +0000

    Lesson 11.43

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,2 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@nomsterapp.com'
-  layout 'mailer'
-
-  def comment_added
-    mail(to: "derek.dunbar@icloud.com",
-      subject: "A comment has been added to your place")
-  end
 end
